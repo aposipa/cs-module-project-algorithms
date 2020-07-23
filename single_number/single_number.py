b65d7f426dd63634289e74bc70194b356e06ffd0
aposipa <andrew.posipanka@gmail.com>
2020-07-23 16:18:43 -0400

    Day 2 improvements MVP

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,20 +2,34 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
-    x = (len(arr) -1)
-    for i in range(x):
-        index = 0
-        # loop through arr
-        while index < (x):
-            if (i != index and arr[i] == arr[index]):
-                break
-            index += 1
-        if (index == (x)): 
-            return arr[i]
+# def single_number(arr):
+#     # Your code here
+#     x = (len(arr) -1)
+#     for i in range(x):
+#         index = 0
+#         # loop through arr
+#         while index < (x):
+#             if (i != index and arr[i] == arr[index]):
+#                 break
+#             index += 1
+#         if (index == (x)): 
+#             return arr[i]
 
-    return -1 #not found
+#     return -1 #not found
+
+# new pass at code:
+def single_number(arr):
+    counts = {}
+    # iterate through the arr
+    for arr in arr: #O(n)
+        if arr not in counts:
+            counts[arr] = 1
+        else:
+            counts[arr] += 1
+
+    for k, v in counts.items(): #O(n)
+        if v == 1:
+            return k
 
 
 if __name__ == '__main__':
